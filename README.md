58d31b8df16203d7aeb02d8bd02670168915e405
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-06-29 16:07:40 +0100

    added more CSS and added npm express

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+## Overview
 This a project using a weather api to GET the current weather conditions from a location.
+YES ✅✅✅✅✅✅
+
+
+## Plan
+create a weather app for walking and camping on dartmoor (maybe add exmoor if possible)
+will take a weather API in and display details from it based on location (longitude and latitude)
+
+what can displayed or unpacked from the api?
+something unique about the way it looks
---
+++ public/main.js
@@ -0,0 +1,80 @@
+const url = "/api"
+
+
+// async function getWeather() {
+//   let response = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=3.545717609421039&appid=${key}&units=metric`);
+//   let data = await response.json();
+// };
+// window.addEventListener("load", () => {
+//   let long;
+//   let lat;
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(position => {
+//       long = position.coords.longitude;
+//       lat = position.coords.latitude;
+//       console.log(`long: ${long}`);
+//       console.log(`lat : ${lat}`);
+//       if (long & lat) {
+//     }})
+//   }
+// });
+
+async function getWeatherDart() {
+    // let long = -3.87634699388993;
+    // let lat = 50.54288694008082;
+    // let response = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`);
+    const response = await fetch(`${url}/dartmeet`);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.payload.main.temp);
+    const dartmeetTemperature = document.querySelector(".dartmeet-temperature"); 
+    dartmeetTemperature.innerText = data.payload.main.temp;
+    const dartmeetIcon = document.querySelector("#dartmeet-weather-icon");
+    dartmeetIcon.innerText = data.payload.weather[0].description;
+};
+
+getWeatherDart();
+
+
+
+
+// async function getWeatherPost() {
+//     let long = -3.904797407096003;
+//     let lat = 50.59543292118994;
+//     let response = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`);
+//     let data = await response.json();
+//     console.log(data);
+//     console.log(data.main.temp);
+//     let postbridgeTemperature = document.querySelector(".postbridge-temperature"); 
+//     postbridgeTemperature.innerText = data.main.temp;
+//     let postbridgeIcon = document.querySelector("#postbridge-weather-icon");
+//     postbridgeIcon.innerText = data.weather[0].description;
+// };
+
+// getWeatherPost();
+
+// async function getWeatherHavford() {
+//     let long = -3.918072245803985;
+//     let lat = 50.418839224119246;
+//     let response = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`);
+//     let data = await response.json();
+//     console.log(data);
+//     console.log(data.main.temp);
+//     let havfordTemperature = document.querySelector(".havford-temperature"); 
+//     havfordTemperature.innerText = data.main.temp;
+//     let havfordIcon = document.querySelector("#havford-weather-icon");
+//     havfordIcon.innerText = data.weather[0].description;
+// };
+
+// // getWeatherHavford();
+
+
+
+/*
+a list of locations long and lat accross dartmoor
+
+Dartmeet = 50.54288694008082, -3.87634699388993
+Ponswrothy = 50.54875187209245, -3.8353971126325623
+Harford = 50.41878569340013, -3.917283233241301
+
+*/
