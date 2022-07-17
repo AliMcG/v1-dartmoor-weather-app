1e944a19f8ee57ba141d1904dd18037cc90fc2e9
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-07-17 10:51:44 +0100

    trying hard-coded key

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const url = "/api"
-const apiKey = process.env.API_KEY
+// const apiKey = process.env.API_KEY
+const apikey = "245d0858a48d16422c1fa7a2ff0ed6ec"
 
 async function getWeatherDart() {
     const long = -3.87634699388993;
