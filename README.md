357d28166b905df96290ed5d59c4475452809576
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-07-19 15:15:49 +0100

    re-written README and added in all api fetch requests

--- README.md
+++ README.md
@@ -1,32 +1,21 @@
-## Overview
+# Dartmoor Weather App v1.0
 This is the first iteration on the initial idea of building a weather app [from a YouTube tutorial](https://www.youtube.com/watch?v=wPElVpR1rwA&list=PLx1f4RjA-BBn2NFhqVCWTfM7NFZ757LHh&index=1&t=1866s).
-The tutorial built an app using the users current location and passed the longitude and latitude to the weather api.
+The tutorial built an app using the user's current location and passed the longitude and latitude to the weather api.
 I decided to build a weather app for Dartmoor National park, I place that I love to go walking in. 
-To see the full functionality of this app please get api key from [Open Weather Map](https://openweathermap.org/api).
+You can visit the this app [here](https://dartmoor-weather-appv1.netlify.app/).
 
-
+The weather app uses weather data from the [Open Weather Map](https://openweathermap.org/api).
 
 ## MVP
-Can get weather for one location on Dartmoor - uses a fetch GET request from "/public/main.js" to the Express() server.✅
-Express server then sends a GET request to weather api using credentials that are stored in .env✅
-(Need to npm i node-fetch to handle fetch requests from Express).✅
+This weather app is version 1.0 in a seires of iterations on the main idea. 
+The M.V.P. for version 1.0 is to get the weather by 6 locations accross [Dartmoor National Park](https://www.dartmoor.gov.uk/).
+The app was written in JavaScript, HTML and CSS. 
+As this repesents the version, 1.0 I did not use React.js or another other higher level functionality.
+Version 1.0 uses vanilla JavaScript as that was the first thing we learnt on the [School of Code Bootcamp](https://www.schoolofcode.co.uk/).
 
-## Testing
-Use jest and supertest to test routes and data structure
+## Protecting the API key
+Although the version 1.0 uses vanilla JavaScript, I had to spin up a server using Express.js so that I could use environment variables.
 
-## Stretch goals
-Can an weather icon be added to each location card?✅
-5 day weather forecast is also availalble from the api for free!
+## Depolyment
+The weather app is hosted on Netlfiy [here](https://dartmoor-weather-appv1.netlify.app/). I ran into some problems deploying the app and protecting the api key. In the end I created a new back-end project to route the api weather data requests through, on which I used the process.env function to hide the sensitive data.
 
-# Dartmoor Wather App version 2 with React (maybe map?)
-## Plan for React App
-Take the concept from the 1st Dartmoor app and expand on it.
-* The first version displayed a card for a location on Dartmoor with the current weather description, weather icon and temperature. Found [here](https://github.com/AliMcG/dartmoor-weather-app-express).
-
-## The React app
-This version will add in 5 day weather forcast to each 'card'. The WeatherCard component will have two "states":
-* The current weather (icon, description and temperature).
-* The 5 day forcast (icon, description and temperature for each day).
-
-### Stretch Goal
-To add in a google map with Marker component. The Marker component will onClick display the weather data.
--- public/main.js
+++ public/main.js
@@ -1,21 +1,11 @@
-// import { apiKey } from "../models";
-const url = "https://salty-depths-59147.herokuapp.com"
-// const apiKey = API_KEY
-// const apiKey = process.env.API_KEY
-// const url = process.env.REACT_APP_API_URL
+const url = "https://salty-depths-59147.herokuapp.com/portfolio"
 
 async function getWeatherDart() {
-    // const long = -3.87634699388993;
-    // const lat = 50.54288694008082;
-    // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`);
-    const response = await fetch(`${url}/portfolio/dartmeet`);
+    
+    const response = await fetch(`${url}/dartmeet`);
     const data = await response.json();
-    console.log(data);
-    // console.log(data.payload.main.temp);
     const iconId = data.payload.weather[0].icon
-    // const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
     document.getElementById('dartmeet-icon').src=`http://openweathermap.org/img/wn/${iconId}@2x.png`;
-    // console.log(res.url);
     const dartmeetTemperature = document.querySelector(".dartmeet-temperature"); 
     dartmeetTemperature.innerText = data.payload.main.temp;
     const dartmeetDescription = document.querySelector("#dartmeet-weather");
@@ -24,89 +14,67 @@ async function getWeatherDart() {
 
 getWeatherDart();
 
-// async function getWeatherPost() {
-//     const response = await fetch(`${url}/postbridge`);
-//     const data = await response.json();
-//     // console.log(data);
-//     // console.log(data.payload.main.temp);
-//     const iconId = data.payload.weather[0].icon
-//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-//     document.getElementById('postbridge-icon').src=`${res.url}`;
-//     console.log(res.url);
-//     const postbridgeTemperature = document.querySelector(".postbridge-temperature"); 
-//     postbridgeTemperature.innerText = data.payload.main.temp;
-//     const postbridgeDescription = document.querySelector("#postbridge-weather");
-//     postbridgeDescription.innerText = data.payload.weather[0].description;
-// };
+async function getWeatherPost() {
+    const response = await fetch(`${url}/postbridge`);
+    const data = await response.json();
+    const iconId = data.payload.weather[0].icon
+    document.getElementById('postbridge-icon').src=`http://openweathermap.org/img/wn/${iconId}@2x.png`;
+    const postbridgeTemperature = document.querySelector(".postbridge-temperature"); 
+    postbridgeTemperature.innerText = data.payload.main.temp;
+    const postbridgeDescription = document.querySelector("#postbridge-weather");
+    postbridgeDescription.innerText = data.payload.weather[0].description;
+};
 
-// getWeatherPost();
+getWeatherPost();
 
-// async function getWeatherHavford() {
-//     const response = await fetch(`${url}/havford`);
-//     const data = await response.json();
-//     const iconId = data.payload.weather[0].icon
-//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-//     document.getElementById('havford-icon').src=`${res.url}`;
-//     const havfordTemperature = document.querySelector(".havford-temperature"); 
-//     havfordTemperature.innerText = data.payload.main.temp;
-//     const havfordDescription = document.querySelector("#havford-weather");
-//     havfordDescription.innerText = data.payload.weather[0].description;
-// };
+async function getWeatherHavford() {
+    const response = await fetch(`${url}/havford`);
+    const data = await response.json();
+    const iconId = data.payload.weather[0].icon
+    document.getElementById('havford-icon').src=`http://openweathermap.org/img/wn/${iconId}@2x.png`;
+    const havfordTemperature = document.querySelector(".havford-temperature"); 
+    havfordTemperature.innerText = data.payload.main.temp;
+    const havfordDescription = document.querySelector("#havford-weather");
+    havfordDescription.innerText = data.payload.weather[0].description;
+};
 
-// getWeatherHavford();
+getWeatherHavford();
 
-// async function getWeatherWalkhampton() {
-//     const response = await fetch(`${url}/walkhampton`);
-//     const data = await response.json();
-//     const iconId = data.payload.weather[0].icon
-//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-//     document.getElementById('walkhampton-icon').src=`${res.url}`;
-//     const walkhamptonTemperature = document.querySelector(".walkhampton-temperature"); 
-//     walkhamptonTemperature.innerText = data.payload.main.temp;
-//     const walkhamptonDescription = document.querySelector("#walkhampton-weather");
-//     walkhamptonDescription.innerText = data.payload.weather[0].description;
-// };
+async function getWeatherDownton() {
+    const response = await fetch(`${url}/downton`);
+    const data = await response.json();
+    const iconId = data.payload.weather[0].icon
+    document.getElementById('downton-icon').src=`http://openweathermap.org/img/wn/${iconId}@2x.png`;
+    const downtonTemperature = document.querySelector(".downton-temperature"); 
+    downtonTemperature.innerText = data.payload.main.temp;
+    const downtonDescription = document.querySelector("#downton-weather");
+    downtonDescription.innerText = data.payload.weather[0].description;
+};
 
-// getWeatherWalkhampton();
+getWeatherDownton();
 
-// async function getWeatherDownton() {
-//     const response = await fetch(`${url}/downton`);
-//     const data = await response.json();
-//     const iconId = data.payload.weather[0].icon
-//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-//     document.getElementById('downton-icon').src=`${res.url}`;
-//     const downtonTemperature = document.querySelector(".downton-temperature"); 
-//     downtonTemperature.innerText = data.payload.main.temp;
-//     const downtonDescription = document.querySelector("#downton-weather");
-//     downtonDescription.innerText = data.payload.weather[0].description;
-// };
+async function getWeatherSouthzeal() {
+    const response = await fetch(`${url}/south-zeal`);
+    const data = await response.json();
+    const iconId = data.payload.weather[0].icon
+    document.getElementById('south-zeal-icon').src=`http://openweathermap.org/img/wn/${iconId}@2x.png`;
+    const southzealTemperature = document.querySelector(".south-zeal-temperature"); 
+    southzealTemperature.innerText = data.payload.main.temp;
+    const southzealDescription = document.querySelector("#south-zeal-weather");
+    southzealDescription.innerText = data.payload.weather[0].description;
+};
 
-// getWeatherDownton();
+getWeatherSouthzeal();
 
-// async function getWeatherSouthzeal() {
-//     const response = await fetch(`${url}/south-zeal`);
-//     const data = await response.json();
-//     const iconId = data.payload.weather[0].icon
-//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-//     document.getElementById('south-zeal-icon').src=`${res.url}`;
-//     const southzealTemperature = document.querySelector(".south-zeal-temperature"); 
-//     southzealTemperature.innerText = data.payload.main.temp;
-//     const southzealDescription = document.querySelector("#south-zeal-weather");
-//     southzealDescription.innerText = data.payload.weather[0].description;
-// };
+async function getWeatherChagford() {
+    const response = await fetch(`${url}/chagford`);
+    const data = await response.json();
+    const iconId = data.payload.weather[0].icon
+    document.getElementById('chagford-icon').src=`http://openweathermap.org/img/wn/${iconId}@2x.png`;
+    const chagfordTemperature = document.querySelector(".chagford-temperature"); 
+    chagfordTemperature.innerText = data.payload.main.temp;
+    const chagfordDescription = document.querySelector("#chagford-weather");
+    chagfordDescription.innerText = data.payload.weather[0].description;
+};
 
-// getWeatherSouthzeal();
-
-// async function getWeatherChagford() {
-//     const response = await fetch(`${url}/chagford`);
-//     const data = await response.json();
-//     const iconId = data.payload.weather[0].icon
-//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-//     document.getElementById('chagford-icon').src=`${res.url}`;
-//     const chagfordTemperature = document.querySelector(".chagford-temperature"); 
-//     chagfordTemperature.innerText = data.payload.main.temp;
-//     const chagfordDescription = document.querySelector("#chagford-weather");
-//     chagfordDescription.innerText = data.payload.weather[0].description;
-// };
-
-// getWeatherChagford();
+getWeatherChagford();
