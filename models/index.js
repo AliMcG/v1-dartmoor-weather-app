8cf01362562f0f666c89b2abc1876b197ee9eb59
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-07-19 09:39:26 +0100

    added url to heroku site to handle api request

--- models/index.js
+++ models/index.js
@@ -1,12 +1,14 @@
 import "dotenv/config"
 import fetch from 'node-fetch';
 
-export const apiKey = process.env.API_KEY
+const url = process.env.API_URL
+// const apiKey = process.env.API_KEY
 
 export async function getDartmeetWeather(){
-  const long = -3.87634699388993;
-  const lat = 50.54288694008082;
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`);
+  // const long = -3.87634699388993;
+  // const lat = 50.54288694008082;
+  // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`);
+  const response = await fetch(`${url}/portfolio/dartmeet`)
   const data = await response.json();
   // console.log(data);
   // console.log(data.main.temp);
