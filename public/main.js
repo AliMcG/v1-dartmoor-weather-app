2666e63df1cc90bf0ff4802b4369fa1026447876
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-07-19 09:44:27 +0100

    added the url to the main.js

--- public/main.js
+++ public/main.js
@@ -1,14 +1,14 @@
 // import { apiKey } from "../models";
-const url = "/api"
-const apiKey = API_KEY
+const url = process.env.API_URL
+// const apiKey = API_KEY
 // const apiKey = process.env.API_KEY
 // const apiKey = "245d0858a48d16422c1fa7a2ff0ed6ec"
 
 async function getWeatherDart() {
-    const long = -3.87634699388993;
-    const lat = 50.54288694008082;
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`);
-    // const response = await fetch(`${url}/dartmeet`);
+    // const long = -3.87634699388993;
+    // const lat = 50.54288694008082;
+    // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`);
+    const response = await fetch(`${url}/portfolio/dartmeet`);
     const data = await response.json();
     console.log(data);
     // console.log(data.payload.main.temp);
@@ -24,89 +24,89 @@ async function getWeatherDart() {
 
 getWeatherDart();
 
-async function getWeatherPost() {
-    const response = await fetch(`${url}/postbridge`);
-    const data = await response.json();
-    // console.log(data);
-    // console.log(data.payload.main.temp);
-    const iconId = data.payload.weather[0].icon
-    const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-    document.getElementById('postbridge-icon').src=`${res.url}`;
-    console.log(res.url);
-    const postbridgeTemperature = document.querySelector(".postbridge-temperature"); 
-    postbridgeTemperature.innerText = data.payload.main.temp;
-    const postbridgeDescription = document.querySelector("#postbridge-weather");
-    postbridgeDescription.innerText = data.payload.weather[0].description;
-};
+// async function getWeatherPost() {
+//     const response = await fetch(`${url}/postbridge`);
+//     const data = await response.json();
+//     // console.log(data);
+//     // console.log(data.payload.main.temp);
+//     const iconId = data.payload.weather[0].icon
+//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
+//     document.getElementById('postbridge-icon').src=`${res.url}`;
+//     console.log(res.url);
+//     const postbridgeTemperature = document.querySelector(".postbridge-temperature"); 
+//     postbridgeTemperature.innerText = data.payload.main.temp;
+//     const postbridgeDescription = document.querySelector("#postbridge-weather");
+//     postbridgeDescription.innerText = data.payload.weather[0].description;
+// };
 
-getWeatherPost();
+// getWeatherPost();
 
-async function getWeatherHavford() {
-    const response = await fetch(`${url}/havford`);
-    const data = await response.json();
-    const iconId = data.payload.weather[0].icon
-    const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-    document.getElementById('havford-icon').src=`${res.url}`;
-    const havfordTemperature = document.querySelector(".havford-temperature"); 
-    havfordTemperature.innerText = data.payload.main.temp;
-    const havfordDescription = document.querySelector("#havford-weather");
-    havfordDescription.innerText = data.payload.weather[0].description;
-};
+// async function getWeatherHavford() {
+//     const response = await fetch(`${url}/havford`);
+//     const data = await response.json();
+//     const iconId = data.payload.weather[0].icon
+//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
+//     document.getElementById('havford-icon').src=`${res.url}`;
+//     const havfordTemperature = document.querySelector(".havford-temperature"); 
+//     havfordTemperature.innerText = data.payload.main.temp;
+//     const havfordDescription = document.querySelector("#havford-weather");
+//     havfordDescription.innerText = data.payload.weather[0].description;
+// };
 
-getWeatherHavford();
+// getWeatherHavford();
 
-async function getWeatherWalkhampton() {
-    const response = await fetch(`${url}/walkhampton`);
-    const data = await response.json();
-    const iconId = data.payload.weather[0].icon
-    const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-    document.getElementById('walkhampton-icon').src=`${res.url}`;
-    const walkhamptonTemperature = document.querySelector(".walkhampton-temperature"); 
-    walkhamptonTemperature.innerText = data.payload.main.temp;
-    const walkhamptonDescription = document.querySelector("#walkhampton-weather");
-    walkhamptonDescription.innerText = data.payload.weather[0].description;
-};
+// async function getWeatherWalkhampton() {
+//     const response = await fetch(`${url}/walkhampton`);
+//     const data = await response.json();
+//     const iconId = data.payload.weather[0].icon
+//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
+//     document.getElementById('walkhampton-icon').src=`${res.url}`;
+//     const walkhamptonTemperature = document.querySelector(".walkhampton-temperature"); 
+//     walkhamptonTemperature.innerText = data.payload.main.temp;
+//     const walkhamptonDescription = document.querySelector("#walkhampton-weather");
+//     walkhamptonDescription.innerText = data.payload.weather[0].description;
+// };
 
-getWeatherWalkhampton();
+// getWeatherWalkhampton();
 
-async function getWeatherDownton() {
-    const response = await fetch(`${url}/downton`);
-    const data = await response.json();
-    const iconId = data.payload.weather[0].icon
-    const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-    document.getElementById('downton-icon').src=`${res.url}`;
-    const downtonTemperature = document.querySelector(".downton-temperature"); 
-    downtonTemperature.innerText = data.payload.main.temp;
-    const downtonDescription = document.querySelector("#downton-weather");
-    downtonDescription.innerText = data.payload.weather[0].description;
-};
+// async function getWeatherDownton() {
+//     const response = await fetch(`${url}/downton`);
+//     const data = await response.json();
+//     const iconId = data.payload.weather[0].icon
+//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
+//     document.getElementById('downton-icon').src=`${res.url}`;
+//     const downtonTemperature = document.querySelector(".downton-temperature"); 
+//     downtonTemperature.innerText = data.payload.main.temp;
+//     const downtonDescription = document.querySelector("#downton-weather");
+//     downtonDescription.innerText = data.payload.weather[0].description;
+// };
 
-getWeatherDownton();
+// getWeatherDownton();
 
-async function getWeatherSouthzeal() {
-    const response = await fetch(`${url}/south-zeal`);
-    const data = await response.json();
-    const iconId = data.payload.weather[0].icon
-    const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-    document.getElementById('south-zeal-icon').src=`${res.url}`;
-    const southzealTemperature = document.querySelector(".south-zeal-temperature"); 
-    southzealTemperature.innerText = data.payload.main.temp;
-    const southzealDescription = document.querySelector("#south-zeal-weather");
-    southzealDescription.innerText = data.payload.weather[0].description;
-};
+// async function getWeatherSouthzeal() {
+//     const response = await fetch(`${url}/south-zeal`);
+//     const data = await response.json();
+//     const iconId = data.payload.weather[0].icon
+//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
+//     document.getElementById('south-zeal-icon').src=`${res.url}`;
+//     const southzealTemperature = document.querySelector(".south-zeal-temperature"); 
+//     southzealTemperature.innerText = data.payload.main.temp;
+//     const southzealDescription = document.querySelector("#south-zeal-weather");
+//     southzealDescription.innerText = data.payload.weather[0].description;
+// };
 
-getWeatherSouthzeal();
+// getWeatherSouthzeal();
 
-async function getWeatherChagford() {
-    const response = await fetch(`${url}/chagford`);
-    const data = await response.json();
-    const iconId = data.payload.weather[0].icon
-    const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
-    document.getElementById('chagford-icon').src=`${res.url}`;
-    const chagfordTemperature = document.querySelector(".chagford-temperature"); 
-    chagfordTemperature.innerText = data.payload.main.temp;
-    const chagfordDescription = document.querySelector("#chagford-weather");
-    chagfordDescription.innerText = data.payload.weather[0].description;
-};
+// async function getWeatherChagford() {
+//     const response = await fetch(`${url}/chagford`);
+//     const data = await response.json();
+//     const iconId = data.payload.weather[0].icon
+//     const res = await fetch(`http://openweathermap.org/img/wn/${iconId}@2x.png`)
+//     document.getElementById('chagford-icon').src=`${res.url}`;
+//     const chagfordTemperature = document.querySelector(".chagford-temperature"); 
+//     chagfordTemperature.innerText = data.payload.main.temp;
+//     const chagfordDescription = document.querySelector("#chagford-weather");
+//     chagfordDescription.innerText = data.payload.weather[0].description;
+// };
 
-getWeatherChagford();
+// getWeatherChagford();
